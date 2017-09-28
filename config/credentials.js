0d3b99f60eaeefdee485dae90e8817664f8d3003
Bernardo Marques Graça <bernardo.marquesg@gmail.com>
2017-09-28 18:07:05 +0100

    Update credentials.js

--- config/credentials.js
+++ config/credentials.js
@@ -3,18 +3,18 @@
 module.exports = {
 
     'Facebook' : {
-        'clientID'        : '1789170698080095', // your App ID
-        'clientSecret'    : '500be87428d1f344558d5402659bb4f2', // your App Secret
+        'clientID'        : 'your App ID',
+        'clientSecret'    : 'your App Secret', 
         'callbackURL'     : '/auth/facebook/callback'
     },
 
     'Google' : {
-        'clientID'         : '740499000624-0qflm6ga36qm4lftl69p648609fqdude.apps.googleusercontent.com',
-        'clientSecret'     : 'MOfU7_fyA0dGKsaEJU5l40XO',
+        'clientID'         : 'your App ID',
+        'clientSecret'     : 'your App Secret',
         'callbackURL'      : '/auth/google/callback'
     },
     'Cookie' : {
-        'secret'           : '12345678987654321'
+        'secret'           : 'your-private-secret'
     }
 
 };
